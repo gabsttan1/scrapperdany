42be72c89451c5ffddd869c38d8aa4bbeccddcab
gabsttan1 <contatransferencias70@gmail.com>
2026-07-06 12:14:27 -0300

    server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ async function scrapeBichoCerto(loteriaInfo) {
         const $ = cheerio.load(html);
         const resultadosDaPagina = [];
         
-        // --- TRAVA DE DATA REAL (BRASÍLIA) ---
+        // --- TRAVA D DATA REAL (BRASÍLIA) ---
         const dataHojeBrasil = new Intl.DateTimeFormat('fr-CA', { 
             timeZone: 'America/Sao_Paulo', 
             year: 'numeric', month: '2-digit', day: '2-digit' 
